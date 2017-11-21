6984c4ee1f77cb818ba391f721825d50fd6b0be5
Armen <danielyan_armen@yahoo.com>
2017-11-21 17:15:04 +0400

    change protocol

--- server.js
+++ server.js
@@ -1,12 +1,3 @@
-/*
-Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
-Licensed under the Amazon Software License (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-
-    http://aws.amazon.com/asl/
-
-or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and limitations under the License.
-*/
 var faker = require('faker');
 var moment = require('moment');
 
@@ -17,7 +8,7 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 var Redis = require('ioredis');
-var redis_address = process.env.REDIS_ADDRESS || 'redis://smarttelemed.mfcuqk.ng.0001.use2.cache.amazonaws.com:6379';
+var redis_address = process.env.REDIS_ADDRESS || 'smarttelemed.mfcuqk.ng.0001.use2.cache.amazonaws.com:6379';
 
 var redis = new Redis(redis_address);
 var redis_subscribers = {};
