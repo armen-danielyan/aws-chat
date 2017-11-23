0f3f39cad982f39ba57b24fb2ee4adfd67883634
Armen <danielyan_armen@yahoo.com>
2017-11-23 14:32:15 +0400

    0.1.6

--- libs/chat.js
+++ libs/chat.js
@@ -27,62 +27,60 @@ const sockets = http => {
     addRedisSubscriber('member_delete');
 
     io.on('connection', socket => {
-        let get_members = redis.hgetall('members').then(redis_members => {
-            let members = {};
-            for (let key in redis_members) {
-                members[key] = JSON.parse(redis_members[key]);
-            }
-            return members;
-        });
-
-        let initialize_member = get_members.then(members => {
-            if (members[socket.id]) {
-                return members[socket.id];
-            }
-
-            let username = faker.fake("{{name.firstName}} {{name.lastName}}"),
-                member = {
-                    socket: socket.id,
-                    username: username
-                };
-
-            return redis.hset('members', socket.id, JSON.stringify(member)).then(() => {
-                return member;
+        let get_members = redis.hgetall('members')
+            .then(redis_members => {
+                let members = {};
+                for (let key in redis_members) {
+                    members[key] = JSON.parse(redis_members[key]);
+                }
+                return members;
             });
-        });
 
-        Promise.all([get_members, initialize_member]).then(values => {
-            let members = values[0],
-                member = values[1];
+        let initialize_member = get_members
+            .then(members => {
+                if (members[socket.id]) {
+                    return members[socket.id];
+                }
 
-            io.emit('member_history', members);
+                let username = faker.fake("{{name.firstName}} {{name.lastName}}"),
+                    member = {
+                        socket: socket.id,
+                        username: username
+                    };
 
-            redis.publish('member_add', JSON.stringify(member));
-
-            socket.on('send', message_text => {
-                let date = moment.now(),
-                    message = JSON.stringify({
-                        date: date,
-                        username: member['username'],
-                        avatar: member['avatar'],
-                        message: message_text
+                return redis.hset('members', socket.id, JSON.stringify(member))
+                    .then(() => {
+                        return member;
                     });
-
-                redis.zadd('messages', date, message);
-                redis.publish('messages', message);
             });
 
-            socket.on('disconnect', () => {
-                redis.hdel('members', socket.id);
-                redis.publish('member_delete', JSON.stringify(socket.id));
+        initialize_member
+            .then(member => {
+                redis.publish('member_add', JSON.stringify(member));
+
+                socket.on('send', message_text => {
+                    let date = moment.now(),
+                        message = JSON.stringify({
+                            date: date,
+                            username: member['username'],
+                            message: message_text
+                        });
+
+                    redis.zadd('messages', date, message);
+                    redis.publish('messages', message);
+                });
+
+                socket.on('disconnect', () => {
+                    redis.hdel('members', socket.id);
+                    redis.publish('member_delete', JSON.stringify(socket.id));
+                });
+            })
+            .catch(reason => {
+                console.log('ERROR: ' + reason);
             });
-        }).catch(reason => {
-            console.log('ERROR: ' + reason);
-        });
     });
 
     return io;
-
 };
 
 module.exports = {sockets: sockets};
