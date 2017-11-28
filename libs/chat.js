5b71b4f507ab125bfc77562623f3f0e7aae628b7
Armen <danielyan_armen@yahoo.com>
2017-11-28 16:28:47 +0400

    0.2.5

--- libs/chat.js
+++ libs/chat.js
@@ -1,9 +1,12 @@
 const socketio = require('socket.io'),
-    faker = require('faker'),
     config = require('config'),
-    moment = require('moment'),
     _ = require('lodash'),
     Redis = require('ioredis');
+
+let redisAddress = config.get('redis').address,
+    redisUserSocket = new Redis(redisAddress),
+    redisUserStack = new Redis(redisAddress),
+    redisMessages = new Redis(redisAddress);
 
 let modelChat = require('../models/chat'),
     modelRoom = require('../models/room');
@@ -20,9 +23,30 @@ const sockets = http => {
             console.log(`${username} logged In`);
 
             socket.username = username;
-            userSocket[username] = socket.id;
 
-            sendUserStack();
+            redisUserSocket.hgetall('userSocket')
+                .then(data => {
+                    userSocket = data;
+                    userSocket[username] = socket.id;
+                    redisUserSocket.hset('userSocket', username, socket.id);
+                })
+                .then(() => {
+                    redisUserStack.hgetall('userStack')
+                        .then(data => {
+                            userStack = data;
+
+                            for (let i in userSocket) {
+                                for (let j in userStack) {
+                                    if (j === i) {
+                                        userStack[j] = "Online";
+                                        redisUserStack.hset('userStack', j, "Online");
+                                    }
+                                }
+                            }
+
+                            ioChat.emit('onlineStack', userStack);
+                        })
+                });
         });
 
         socket.on('set-room', toUser => {
@@ -32,15 +56,18 @@ const sockets = http => {
                 currentRoom = username + "-" + toUser,
                 reverseRoom = toUser + "-" + username;
 
-            getRoomData({name1: currentRoom, name2: reverseRoom})
-                .then(roomId => {
-                    socket.room = roomId;
-                    socket.join(socket.room);
-                    return ioChat.to(userSocket[username]).emit('set-room', socket.room);
-                })
-                .catch(error => {
-                    console.log(error);
-                });
+            getRoomData({
+                name1: currentRoom,
+                name2: reverseRoom
+            })
+            .then(roomId => {
+                socket.room = roomId;
+                socket.join(socket.room);
+                return ioChat.to(userSocket[username]).emit('set-room', socket.room);
+            })
+            .catch(error => {
+                console.log(error);
+            });
         });
 
         socket.on('typing', () => {
@@ -63,41 +90,42 @@ const sockets = http => {
                 room: socket.room,
                 date: data.date
             })
-                .then(msgId => {
-                    ioChat.to(socket.room).emit('chat-msg', {
-                        msgFrom: socket.username,
-                        msg: data.msg,
-                        msgId: msgId,
-                        date: data.date
-                    });
-                })
-                .catch(error => {
-                    console.log(error);
+            .then(msgId => {
+                ioChat.to(socket.room).emit('chat-msg', {
+                    msgFrom: socket.username,
+                    msg: data.msg,
+                    msgId: msgId,
+                    date: data.date
                 });
+
+                redisMessages.hset('messages', msgId, JSON.stringify({
+                    msgFrom: socket.username,
+                    msgTo: data.msgTo,
+                    msg: data.msg,
+                    room: socket.room,
+                    date: data.date
+                }));
+            })
+            .catch(error => {
+                console.log(error);
+            });
         });
 
         socket.on('disconnect', () => {
-            console.log(`${socket.username} logged out`);
+            let username = socket.username;
+
+            console.log(`${username} logged out`);
             console.log("chat disconnected.");
 
-            _.unset(userSocket, socket.username);
-            userStack[socket.username] = "Offline";
+            _.unset(userSocket, username);
+            redisUserSocket.hdel('userSocket', username);
+
+            userStack[username] = "Offline";
+            redisUserStack.hset('userStack', username, "Offline");
 
             ioChat.emit('onlineStack', userStack);
         });
     });
-
-    let sendUserStack = () => {
-        for (let i in userSocket) {
-            for (let j in userStack) {
-                if (j === i) {
-                    userStack[j] = "Online";
-                }
-            }
-        }
-
-        ioChat.emit('onlineStack', userStack);
-    };
 
     let saveChat = data => {
         return new Promise((resolve, reject) => {
@@ -122,12 +150,13 @@ const sockets = http => {
     let getRoomData = room => {
         return new Promise((resolve, reject) => {
             let newRoom = new modelRoom();
-            newRoom.query(qb => {
-                qb.where('name1', '=', room.name1)
-                    .orWhere('name1', '=', room.name2)
-                    .orWhere('name2', '=', room.name1)
-                    .orWhere('name2', '=', room.name2);
-            })
+            newRoom
+                .query(qb => {
+                    qb.where('name1', '=', room.name1)
+                        .orWhere('name1', '=', room.name2)
+                        .orWhere('name2', '=', room.name1)
+                        .orWhere('name2', '=', room.name2);
+                })
                 .fetch()
                 .then(model => {
                     if (!model) {
@@ -151,77 +180,6 @@ const sockets = http => {
         });
 
     };
-
-
-    /*let redisAddress = config.get('redis').address,
-        redis = new Redis(redisAddress),
-        redisSubscribers = {};
-
-    function addRedisSubscriber(subscriber_key) {
-        let client = new Redis(redisAddress);
-
-        client.subscribe(subscriber_key);
-        client.on('message', (channel, message) => {
-            io.emit(subscriber_key, JSON.parse(message));
-        });
-
-        redisSubscribers[subscriber_key] = client;
-    }
-
-    addRedisSubscriber('member_add');
-    addRedisSubscriber('member_delete');
-
-    io.on('connection', socket => {
-        let get_members = redis.hgetall('members')
-            .then(redis_members => {
-                let members = {};
-                for (let key in redis_members) {
-                    members[key] = JSON.parse(redis_members[key]);
-                }
-                return members;
-            });
-
-        let initialize_member = get_members
-            .then(members => {
-                if (members[socket.id]) {
-                    return members[socket.id];
-                }
-
-                let member = {
-                        socket: socket.id,
-                        username: faker.fake("{{name.firstName}} {{name.lastName}}")
-                    };
-
-                return redis.hset('members', socket.id, JSON.stringify(member))
-                    .then(() => {
-                        return member;
-                    });
-            });
-
-        initialize_member
-            .then(member => {
-                redis.publish('member_add', JSON.stringify(member));
-
-                socket.on('send', message_text => {
-                    let date = moment.now(),
-                        message = {
-                            date: date,
-                            username: member['username'],
-                            message: message_text
-                        };
-
-                    io.emit('messages', message);
-                });
-
-                socket.on('disconnect', () => {
-                    redis.hdel('members', socket.id);
-                    redis.publish('member_delete', JSON.stringify(socket.id));
-                });
-            })
-            .catch(reason => {
-                console.log('ERROR: ' + reason);
-            });
-    });*/
 
     return io;
 };
